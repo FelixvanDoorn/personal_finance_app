00a381c873031fc688fb4089f7f63962288b5742
Felix van Doorn <felixvandoorn@Felixs-MacBook-Pro.local>
2023-07-24 21:13:17 +0200

    First step for modularizing UI code

--- tab.py
+++ tab.py
@@ -2,12 +2,23 @@ from tkinter import ttk
 
 
 class Tab:
-
     def __init__(self, tab_controller, tab_name) -> None:
         self.tab_name = tab_name
         self.tab = ttk.Frame(tab_controller)
-        self.label = ttk.Label(self.tab,
-                               text="This is the %s section" % self.tab_name)
+
+        self.label = ttk.Label(
+            self.tab, text="Placeholder for %s section" % self.tab_name
+        )
+
         self.label.pack(padx=10, pady=10)
-        tab_controller.add(self.tab,
-                           text="This is the %s section" % self.tab_name)
+        tab_controller.add(self.tab, text=self.tab_name)
+
+
+class BudgetTab(Tab):
+    def __init__(self, tab_controller) -> None:
+        super().__init__(tab_controller, "Budget")
+
+
+class AssetTab(Tab):
+    def __init__(self, tab_controller) -> None:
+        super().__init__(tab_controller, "Assets")
