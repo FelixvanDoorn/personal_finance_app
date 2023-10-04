84870a112b6b83aa97cae145c8d21f21abb33f74
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2023-10-04 20:45:19 +0200

    Setting up controler for saving data

---
+++ controllers/data_entry_controller.py
@@ -0,0 +1,6 @@
+class DataEntryController:
+    def __init__(self):
+        pass
+
+    def save_data():
+        print("Save Data")
--- views/data_entry.py
+++ views/data_entry.py
@@ -1,5 +1,7 @@
 from tkinter import ttk
 
+import tkinter as tk
+
 
 class DataEntry:
     """
@@ -12,19 +14,23 @@ class DataEntry:
     """
 
     def __init__(self, tab_frame) -> None:
+        self.name_var = tk.StringVar()
+        self.value_var = tk.StringVar()
+        self.date_var = tk.StringVar()
+        self.type_var = tk.StringVar()
         self.name_label = ttk.Label(tab_frame, text="Entry Name")
         self.name_label.pack()
-        self.name_entry = ttk.Entry(tab_frame)
+        self.name_entry = ttk.Entry(tab_frame, textvariable=self.name_var)
         self.name_entry.pack()
         self.value_label = ttk.Label(tab_frame, text="Entry Value")
         self.value_label.pack()
-        self.value_entry = ttk.Entry(tab_frame)
+        self.value_entry = ttk.Entry(tab_frame, textvariable=self.value_var)
         self.value_entry.pack()
         self.date_label = ttk.Label(tab_frame, text="Entry Date")
         self.date_label.pack()
-        self.date_entry = ttk.Entry(tab_frame)
+        self.date_entry = ttk.Entry(tab_frame, textvariable=self.date_var)
         self.date_entry.pack()
         self.type_label = ttk.Label(tab_frame, text="Entry Type")
         self.type_label.pack()
-        self.type_entry = ttk.Entry(tab_frame)
+        self.type_entry = ttk.Entry(tab_frame, textvariable=self.type_var)
         self.type_entry.pack()
