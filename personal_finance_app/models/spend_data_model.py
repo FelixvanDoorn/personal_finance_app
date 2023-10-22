f2644e19bb466a10e180279a9992fc486defa4ff
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2023-10-22 18:07:06 +0200

    Start to insertion logic

--- personal_finance_app/models/spend_data_model.py
+++ personal_finance_app/models/spend_data_model.py
@@ -4,11 +4,24 @@ class SpendDataModel:
         self.database_service.run_query(
             """
             CREATE TABLE IF NOT EXISTS spending_data(
-                transaction_id INTEGER PRIMARY KEY,
+                transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 transaction_description TEXT NOT NULL,
                 transaction_amount DOUBLE NOT NULL,
                 transaction_date DATE NOT NULL,
                 transaction_category TEXT
             );
+            """
+        )
+
+    def create_new_transaction(self):
+        self.database_service.run_query(
+            """
+            INSERT INTO spending_data(
+                transaction_id,
+                transaction_description,
+                transaction_amount,
+                transaction_date,
+                transaction_category
+            )
             """
         )
