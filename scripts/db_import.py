36121cb39cba74261f0f1a3d9998ba3b742c87d8
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2023-12-19 08:22:56 +0100

    Script now parses spreadsheet into single DF ready for import

--- scripts/db_import.py
+++ scripts/db_import.py
@@ -1,10 +1,13 @@
-from utils import parse_budgetting_spread_sheet
+from utils import parse_budgetting_spread_sheet, parse_dates
 
 import pandas as pd
 
+# This script imports existing bugetting spreadsheets into DB
+# Formatting is specific for what I've been using for a few years. 
 
 budget_sheet_frame_dict = pd.read_excel(
-    "data/raw_data_files/Finances.xlsx", sheet_name=None
+    "data/raw_data_files/Finances.xlsx",
+    sheet_name=None
 )
 
 sheet_name_list = list(budget_sheet_frame_dict.keys())
@@ -16,5 +19,15 @@ parsed_budget_sheet_frame_dict = {
     for sheet_name in relevant_sheet_name_list
 }
 
+budget_month_list = parsed_budget_sheet_frame_dict.keys()
 
-print(parsed_budget_sheet_frame_dict)
+for budget_month in budget_month_list:
+    transaction_datestr = parse_dates(budget_month)
+    temp_df = parsed_budget_sheet_frame_dict.get(budget_month)
+    temp_df.loc[:, "transaction_date"] = transaction_datestr
+    parsed_budget_sheet_frame_dict[budget_month] = temp_df
+
+budget_sheet_frame_list = parsed_budget_sheet_frame_dict.values()
+total_budget_sheet_frame = pd.concat(budget_sheet_frame_list).reset_index().drop(columns=["index"])
+
+print(total_budget_sheet_frame.tail())
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,79 @@
+from datetime import datetime
+
 import pandas as pd
+import re
+
+
+def add_transaction_categories(budget_category_frame: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds transaction categories to dataframe
+
+    Args: budget_category_frame, a pandas Dataframe
+    Represents a month of transactions, with categories split by columns
+    """
+    category_name = budget_category_frame.iloc[0, 0]
+    category_name = category_name.replace("Expenses", "Spending:")
+    result_frame = budget_category_frame.copy()
+    result_frame.columns = ["transaction_description", "transaction_amount"]
+    result_frame["transaction_category"] = category_name
+    result_frame = result_frame.iloc[1:]
+    return result_frame
 
 
 def parse_budgetting_spread_sheet(budget_frame: pd.DataFrame) -> pd.DataFrame:
-    transactions_df = budget_frame.iloc[8:]
-    return transactions_df
+    transactions_df = budget_frame.iloc[8:].reset_index()
+
+    col_pairs = [
+        ("Post", "Total Income"),
+        ("Investments", "Unnamed: 5"),
+        ("Unnamed: 8", "Unnamed: 9"),
+        ("Unnamed: 12", "Unnamed: 13"),
+    ]
+
+    frame_list = [transactions_df.loc[:, cols] for cols in col_pairs]
+
+    frames_with_transaction_categories = [
+        add_transaction_categories(frame) for frame in frame_list
+    ]
+    total_frame = pd.concat(frames_with_transaction_categories).dropna()
+    total_frame = total_frame.reset_index().drop(columns=["index"])
+
+    return total_frame
+
+
+def find_first_numeric_char_position(datestr: str) -> int:
+    str_match = re.search(r'\d', datestr)
+
+    if str_match:
+        str_match_index = str_match.start()
+    else:
+        raise Exception("No numeric values found")
+    return str_match_index
+
+
+def space_datestr(datestr: str, index: int) -> str:
+    spaced_datestr = None
+
+    if index < len(datestr):
+        if datestr[index-1] == " ":
+            spaced_datestr = datestr 
+        else:
+            spaced_datestr = datestr[:index] + " " + datestr[index:]
+    else:
+        raise Exception("Cannot space string at this index")
+    
+    return spaced_datestr
+        
+
+
+def parse_dates(datestr: str) -> str:
+    index_to_space = find_first_numeric_char_position(datestr)
+    spaced_datestr = space_datestr(datestr, index_to_space)
+    date = None
+
+    try:
+        date = datetime.strptime(spaced_datestr, "%b %Y")
+    except ValueError:
+        date = datetime.strptime(spaced_datestr, "%B %Y")
+
+    return date.strftime("%Y-%m-%d")
